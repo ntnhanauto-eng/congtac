fc07ee17fef9c52fe747221fd0643924f5f76059
ntnhanauto-eng <ntnhan.auto@gmail.com>
2026-05-30 15:50:40 +0700

    Create tuya_config.php

---
+++ tuya_config.php
@@ -0,0 +1,30 @@
+<?php
+// tuya_config.php
+define('TUYA_CLIENT_ID', 'qap98nweqkmufpdp5d3r');
+define('TUYA_SECRET', 'cb7684adc56045bdb5f77c1d7a541d48');
+define('TUYA_API_URL', 'https://openapi.tuyaus.com'); // Giữ nguyên nếu ở khu vực Châu Á/Mỹ
+
+function getTuyaToken() {
+    $timestamp = time() * 1000;
+    $signUrl = '/v1.0/token?grant_type=1';
+    
+    // Tạo chữ ký bảo mật theo thuật toán của Tuya
+    $stringToSign = TUYA_CLIENT_ID . $timestamp . "GET\n" . "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\n" . "\n" . $signUrl;
+    $sign = strtoupper(hash_hmac('sha256', $stringToSign, TUYA_SECRET));
+
+    $ch = curl_init(TUYA_API_URL . $signUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "client_id: " . TUYA_CLIENT_ID,
+        "sign: " . $sign,
+        "t: " . $timestamp,
+        "sign_method: HMACS256"
+    ]);
+    
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $resData = json_decode($response, true);
+    
+    return $resData['result']['access_token'] ?? null;
+}
+?>
